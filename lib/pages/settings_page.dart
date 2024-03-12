be6332120fa9ed2608aa0b61a395c472a5bc3cb5
asfDRT <vadimv281@gmail.com>
2024-03-12 22:54:31 +0500

    final round

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,16 +1,98 @@
 // login_page.dart
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
-        backgroundColor: Colors.pink[100],
+        title: const Text(
+          'Настройки',
+          style: TextStyle(fontSize: 30.0),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange[200],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+        children: [
+          ListTile(
+            title: Text('Изменить пароль', style: TextStyle(fontSize: 20.0)),
+            leading: Icon(Icons.lock),
+            onTap: () {
+              // Анимация при нажатии
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Уведомления', style: TextStyle(fontSize: 20.0)),
+            leading: Icon(Icons.notifications),
+            trailing: Switch(
+              value: true,
+              onChanged: (bool value) {
+                // Логика для включения/отключения уведомлений
+              },
+            ),
+          ),
+          ListTile(
+            title: Text('Изменить профиль', style: TextStyle(fontSize: 20.0)),
+            leading: Icon(Icons.person),
+            onTap: () {
+              // Анимация при нажатии
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Настройки приватности', style: TextStyle(fontSize: 20.0)),
+            leading: Icon(Icons.lock_outline),
+            onTap: () {
+              // Анимация при нажатии
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Удалить аккаунт', style: TextStyle(fontSize: 20.0)),
+            leading: Icon(Icons.delete),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Подтверить удаление аккаунта'),
+                    content: Text('Вы уверены?'),
+                    actions: [
+                      TextButton(
+                        child: Text('Отмена'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      TextButton(
+                        child: Text('Удалить'),
+                        onPressed: () {
+                          // Логика для удаления аккаунта
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
