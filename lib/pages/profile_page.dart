734ed57e622aaa633b0774b440ba309b769ff8d5
asfDRT <vadimv281@gmail.com>
2024-03-12 18:30:42 +0500

    Fir commit from vadim

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -8,6 +8,9 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
     );
   }
 }
